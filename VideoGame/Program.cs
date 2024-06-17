524637900e7abe63f945c9fa01b299143c7897d3
Helena Miura <helenamiura@gmail.com>
2024-06-17 10:27:21 +0200

    added character class

--- VideoGame/Program.cs
+++ VideoGame/Program.cs
@@ -1,2 +1,26 @@
-﻿
+﻿public class Character {
+    private string name;
+    private int lifePoints;
+    private int forceAtack;
+    private int forceDefense;
 
+//constructor
+    public Character (string name, int lifePoints, int forceAtack, int forceDefense){
+        this.name = name;
+        this.lifePoints = lifePoints;
+        this.forceAtack = forceAtack;
+        this.forceDefense = forceDefense;
+    }
+
+// calculate if the character is alive
+    public bool IsAlive(){
+        bool alive = lifePoints > 0 ? true : false;
+        return alive;
+    }
+    
+// method to calculate the life points of the attacked character
+    public int Attack (Character attacked, Character attacker){
+        attacked.lifePoints = attacked.lifePoints - (attacker.forceAtack - attacked.forceDefense);
+        return lifePoints;
+    }
+}
